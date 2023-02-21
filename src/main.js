eee41452becc01578bf676f3b0563aa22a5093ff
PhatHH <haphat0707@gmai.coml>
2023-02-21 22:09:30 +0700

    feat(master): build core

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import i18n from "@/core/plugins/vue-i18n";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+// router.beforeEach((to, from, next) => {
+//     // Ensure we checked auth before each page load.
+//     Promise.all([store.dispatch(VERIFY_AUTH)]).then(next);
 
+//     // reset config to initial state
+//     store.dispatch(RESET_LAYOUT_CONFIG);
+
+//     // Scroll page to top on every route change
+//     setTimeout(() => {
+//       window.scrollTo(0, 0);
+//     }, 100);
+//   });
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+  i18n,
+}).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,88 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      redirect: "/dashboard",
+      //   component: () => import("@/view/layout/Layout"),
+      children: [
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          //   component: () => import("@/view/pages/Dashboard.vue"),
+        },
+        {
+          path: "/builder",
+          name: "builder",
+          //   component: () => import("@/view/pages/Builder.vue"),
+        },
+        {
+          path: "/vue-bootstrap",
+          name: "vue-bootstrap",
+          //   component: () => import("@/view/pages/vue-bootstrap/VueBootstrap.vue"),
+          children: [
+            {
+              path: "alert",
+              name: "vue-bootstrap-alert",
+              //   component: () => import("@/view/pages/vue-bootstrap/Alert.vue"),
+            },
+          ],
+        },
+        {
+          path: "/profile",
+          name: "profile",
+          //   component: () => import("@/view/pages/profile/Profile.vue"),
+          children: [
+            {
+              path: "profile-1",
+              name: "profile-1",
+              //   component: () => import("@/view/pages/profile/Profile-1.vue"),
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/custom-error",
+      name: "error",
+      //   component: () => import("@/view/pages/error/Error.vue"),
+      children: [
+        {
+          path: "error-1",
+          name: "error-1",
+          //   component: () => import("@/view/pages/error/Error-1.vue"),
+        },
+      ],
+    },
+    {
+      path: "/",
+      //   component: () => import("@/view/pages/auth/login_pages/Login-1"),
+      children: [
+        {
+          name: "login",
+          path: "/login",
+          //   component: () => import("@/view/pages/auth/login_pages/Login-1"),
+        },
+        {
+          name: "register",
+          path: "/register",
+          //   component: () => import("@/view/pages/auth/login_pages/Login-1"),
+        },
+      ],
+    },
+    {
+      path: "*",
+      redirect: "/404",
+    },
+    {
+      // the 404 route, when none of the above matches
+      path: "/404",
+      name: "404",
+      //   component: () => import("@/view/pages/error/Error-1.vue"),
+    },
+  ],
+});
